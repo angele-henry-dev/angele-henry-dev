6c7d7ac1c70808924a02788166c386c1a7b73bfb
Angele <59701731+AngeleCode@users.noreply.github.com>
2023-01-17 22:52:38 +0100

    Add background image

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
