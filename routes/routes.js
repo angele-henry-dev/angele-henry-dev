c4ccd03996b2d19837192d68477634e6aeeb8ce7
Angele <angele.henry@outlook.com>
2023-01-26 12:00:11 +0100

    Move the menu

--- routes/routes.js
+++ routes/routes.js
@@ -8,4 +8,9 @@ router.get('/', function (req, res) {
     res.render('index');
 });
 
+// POST message
+router.get('/contact', function (req, res) {
+    res.send(200, { message: 'mailto:angele.henry@outlook.com' });
+});
+
 module.exports = router;
