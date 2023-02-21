c0d85192bdbb04bf582f6b2299679513134cc073
Angele <angele.henry@outlook.com>
2023-02-21 12:43:55 +0100

    Dynamic text

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ let app = express();
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
-
 app.use((req, res, next) => {
     next();
 });
@@ -21,18 +20,6 @@ app.get('/404', function (req, res, next) {
     // will match /404 after this one, and we're not
     // responding here
     next();
-});
-
-app.get('/403', function (req, res, next) {
-    // trigger a 403 error
-    var err = new Error('not allowed!');
-    err.status = 403;
-    next(err);
-});
-
-app.get('/500', function (req, res, next) {
-    // trigger a generic (500) error
-    next(new Error('keyboard cat!'));
 });
 
 // Error handlers
@@ -54,14 +41,6 @@ app.use(function (req, res, next) {
             res.type('txt').send('Not found')
         }
     })
-});
-
-app.use(function (err, req, res, next) {
-    // we may use properties of the error object
-    // here and next(err) appropriately, or if
-    // we possibly recovered from the error, simply next().
-    res.status(err.status || 500);
-    res.render('500', { error: err });
 });
 
 app.listen(port, () => { });
--- routes/routes.js
+++ routes/routes.js
@@ -3,14 +3,22 @@
 var express = require('express');
 var router = express.Router();
 
+// Define models
+const skillsModel = require('../model/skills.json');
+const experienceModel = require('../model/experience.json');
+
 // GET home page
 router.get('/', function (req, res) {
-    res.render('index');
+    const data = {
+        skills: skillsModel,
+        experience: experienceModel
+    };
+    res.render('index', data);
 });
 
 // POST message
 router.get('/contact', function (req, res) {
-    res.send(200, { message: 'mailto:angele.henry@outlook.com' });
+    res.status(200).send({ message: 'mailto:angele.henry@outlook.com' });
 });
 
 module.exports = router;
