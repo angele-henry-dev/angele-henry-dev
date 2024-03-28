5cf4cc0686d83602e40877259db5176c7b49f677
Angèle Henry <angele.henry@outlook.com>
2024-03-28 16:53:23 +0100

    Add projects

--- js/index.js
+++ js/index.js
@@ -1,85 +1,67 @@
 import skills from '/model/skills.json' with { type: 'json' };
+import projects from '/model/projects.json' with { type: 'json' };
 import experience from '/model/experience.json' with { type: 'json' };
+
 generateSkills(skills);
 generateExperience(experience);
+generateProjects(projects);
+
+function createElement(type, classNames, attributes = {}) {
+    const element = document.createElement(type);
+    if (classNames) {
+        element.classList.add(...classNames.split(' '));
+    }
+    Object.keys(attributes).forEach(attr => element.setAttribute(attr, attributes[attr]));
+    return element;
+}
+
+function createTextElement(tag, classList, text) {
+    const element = createElement(tag, classList);
+    element.innerHTML = text;
+    return element;
+}
+
+function addTextContent(element, text) {
+    element.textContent = text;
+    return element;
+}
+
+function createList(technologies) {
+    const list = document.createElement("ul");
+    list.classList.add("list-group", "list-group-flush");
+    technologies.forEach(technology => {
+        const item = document.createElement("li");
+        item.classList.add("list-group-item");
+        list.appendChild(addTextContent(createElement("li", "list-group-item"), technology));
+    });
+    return list;
+}
 
 function generateSkills(skills) {
     const skillsElement = document.querySelector("#skills");
-
-    function createCardElementWithClasses(classList) {
-        const element = document.createElement("div");
-        element.classList.add(...classList);
-        return element;
-    }
-
-    function createImageElement(skill) {
-        const image = document.createElement("img");
-        image.src = skill.imagePath;
-        image.alt = skill.title;
-        return image;
-    }
-
-    function createTextElement(tag, classList, text) {
-        const element = document.createElement(tag);
-        element.classList.add(...classList);
-        element.innerHTML = text;
-        return element;
-    }
-
-    function createList(technologies) {
-        const list = document.createElement("ul");
-        list.classList.add("list-group", "list-group-flush");
-        technologies.forEach(technology => {
-            const item = document.createElement("li");
-            item.classList.add("list-group-item");
-            item.textContent = technology;
-            list.appendChild(item);
-        });
-        return list;
-    }
-
-    for (const skill of skills) {
-        const cardItem = createCardElementWithClasses(["card-angele", "rounded", "mb-3", "mx-1"]);
+    skills.forEach(skill => {
+        const cardItem = createElement("div", "card-custom rounded mb-3 mx-1");
         
-        const cardEntete = createCardElementWithClasses(["card-entete"]);
-        cardEntete.appendChild(createCardElementWithClasses(["black-cover"]));
-        cardEntete.appendChild(createImageElement(skill));
+        const cardEntete = createElement("div", "card-entete");
+        cardEntete.appendChild(createElement("div", "black-cover"));
+        cardEntete.appendChild(createElement("img", null, {"src": skill.imagePath, "alt": skill.title}));
 
-        const titleDiv = createCardElementWithClasses(["p-3"]);
-        titleDiv.appendChild(createTextElement("h2", ["h5"], skill.title));
-        titleDiv.appendChild(createTextElement("span", ["small", "text-gray"], skill.subtitle));
+        const titleDiv = createElement("div", "p-3");
+        titleDiv.appendChild(createTextElement("h2", "h5", skill.title));
+        titleDiv.appendChild(createTextElement("span", "small text-gray", skill.subtitle));
         cardEntete.appendChild(titleDiv);
 
-        const cardContent = createCardElementWithClasses(["card-content", "p-3"]);
-        cardContent.appendChild(createTextElement("h6", [], skill.technologiesTitle));
+        const cardContent = createElement("div", "card-content p-3");
+        cardContent.appendChild(createTextElement("h6", null, skill.technologiesTitle));
         cardContent.appendChild(createList(skill.technologies));
 
         cardItem.appendChild(cardEntete);
         cardItem.appendChild(cardContent);
         skillsElement.appendChild(cardItem);
-    }
+    });
 }
 
 function generateExperience(experience) {
-    function createElement(type, classNames, attributes = {}) {
-        const element = document.createElement(type);
-        if (classNames) {
-            element.classList.add(...classNames.split(' ')); // Split by space and apply all
-        }
-        Object.keys(attributes).forEach(attr => element.setAttribute(attr, attributes[attr]));
-        return element;
-    }
-    
-    function addTextContent(element, text) {
-        element.textContent = text;
-        return element;
-    }
-    
-    function addInnerHTML(element, html) {
-        element.innerHTML = html;
-        return element;
-    }
-
     const experienceElement = document.querySelector("#experience");
     experience.forEach(exp => {
         const flipCard = createElement("div", "flip-card mb-3");
@@ -96,16 +78,16 @@ function generateExperience(experience) {
     });
 
     function createCardSide(exp, isFront) {
-        const side = createElement("div", `flip-card-${isFront ? 'front' : 'back'} card-angele rounded`);
+        const side = createElement("div", `flip-card-${isFront ? 'front' : 'back'} card-custom rounded`);
         const cardHeader = createElement("div", "card-entete");
         cardHeader.appendChild(createElement("div", "black-cover"));
         cardHeader.appendChild(createElement("img", null, {src: exp.imagePath, alt: exp.what}));
         const textContainer = createElement("div", "p-3");
         textContainer.appendChild(addTextContent(createElement("h2", "h5"), exp.what));
         if (isFront) {
-            textContainer.appendChild(addInnerHTML(createElement("span", "small text-gray"), `<i class="fa fa-clock-o me-2"></i>${exp.when}`));
+            textContainer.appendChild(createTextElement("span", "small text-gray", `<i class="fa fa-clock-o me-2"></i>${exp.when}`));
         } else {
-            textContainer.appendChild(addInnerHTML(createElement("span", "small text-gray"), `<i class="fa fa-regular fa-floppy-disk me-2"></i>${exp.technologiesTitle}`));
+            textContainer.appendChild(createTextElement("span", "small text-gray", `<i class="fa fa-regular fa-floppy-disk me-2"></i>${exp.technologiesTitle}`));
         }
         cardHeader.appendChild(textContainer);
         side.appendChild(cardHeader);
@@ -127,4 +109,29 @@ function generateExperience(experience) {
         }
         return side;
     }
+}
+
+function generateProjects(projects) {
+    const projectsElement = document.querySelector("#projects");
+    projects.forEach(project => {
+        const cardItem = createElement("div", "carousel-custom d-flex flex-column align-items-center");
+        
+        const cardImage = createElement("div", "card-image mb-3");
+        cardImage.appendChild(createElement("img", null, {"src": project.imagePath, "alt": project.title, "height": "400px"}));
+
+        const cardContent = createElement("div", "card-custom card-content p-3 rounded");
+        const titleDiv = createElement("div", null);
+        titleDiv.appendChild(createTextElement("h2", "h5", project.title));
+        titleDiv.appendChild(createTextElement("span", "small text-gray", project.subtitle));
+        cardContent.appendChild(titleDiv);
+        project.descriptions.forEach(description => {
+            cardContent.appendChild(addTextContent(createElement("p", "text-small font-weight-light"), description));
+        });
+        /*cardContent.appendChild(createTextElement("h6", null, project.technologiesTitle));
+        cardContent.appendChild(createList(project.technologies));*/
+
+        cardItem.appendChild(cardImage);
+        cardItem.appendChild(cardContent);
+        projectsElement.appendChild(cardItem);
+    });
 }
