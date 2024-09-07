0d42781ce09e36e8a3a0f4a9a466f2bc244e3d54
Angèle Henry <angele.henry@outlook.com>
2024-09-07 20:42:59 +0200

    Save

--- js/csstricks.js
+++
@@ -1,7 +0,0 @@
-import cssArticles from '/model/css_articles.json' with { type: 'json' };
-
-generateCssArticles(cssArticles);
-
-function generateCssArticles(cssArticles) {
-    const skillsElement = document.querySelector("#cssarticles");
-}
